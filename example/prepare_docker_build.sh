72a49b37c47dfda0978a2ff88e54619bd2812fe7
Michael Frister <michael+git@frister.net>
2014-09-04 09:57:15 +0200

    Example: Run bundle package only on Gemfile change
    If you can cache your vendor/ directory between runs, this allows you
    to skip the package step if neither Gemfile nor Gemfile.lock have
    changed.
    
    This also allows using the Docker cache for building the image when
    using Gems fetched via Git. `bundle package` for some reason updates
    the Git repositories' main folder's change time, so Docker does a full
    rebuild. When not doing `bundle package`, the Git repositories don't
    change and the Docker cache works.

--- example/prepare_docker_build.sh
+++ example/prepare_docker_build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Requires Bundler >= 1.8 (not released as of 2014-09-01) for --no-install
+
 set -e
 
-bundle package --all
+# Only run `bundle package [...]` if Gemfile or Gemfile.lock have changed.
+# `bundle package` unfortunately changes git repository folders on each run
+# so running this always invalidates the Docker cache each time and makes it
+# useless.
+GEMFILES='Gemfile Gemfile.lock'
+GEMFILE_STATE_FILE="vendor/cache-Gemfile-state"
+GEMFILE_STATE=$(cat $GEMFILES | openssl sha1)
+
+if [ "$GEMFILE_STATE" == "$(cat $GEMFILE_STATE_FILE)" ]; then
+    echo "'$GEMFILES' unchanged, skipping "'`bundle package`.'
+else
+    echo "'$GEMFILES' changed, running "'`bundle package`...'
+    bundle package --all --no-install
+    echo "$GEMFILE_STATE" > "$GEMFILE_STATE_FILE"
+fi
