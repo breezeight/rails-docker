7b07304525760bd37538e182bdf41d130aeeb779
Michael Frister <michael+git@frister.net>
2014-09-04 11:30:35 +0200

    sanitize hosts: Skip for Docker < 1.2.0
    /etc/hosts is only writable on Docker >= 1.2.0.
    
    This fixes container startup on older Docker versions.

--- docker/sanitize_hosts.sh
+++ docker/sanitize_hosts.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-cat /etc/hosts | grep '_' | sed 's/_/-/g' >> /etc/hosts
+# Only do this if /etc/hosts is writable (Docker < 1.2.0 compatibility)
+if [ -w /etc/hosts ]; then
+    cat /etc/hosts | grep '_' | sed 's/_/-/g' >> /etc/hosts
+fi
